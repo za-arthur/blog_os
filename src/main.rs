204f5d8392b3a3364293cf0c40c57a0c3a891d40
Arthur Zakirov <z-arthur@yandex.ru>
2019-08-14 18:36:18 +0300

    post-04

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,36 @@
 #![no_std]
 #![no_main]
 
-use core::panic::PanicInfo;
+#![feature(custom_test_frameworks)]
+#![test_runner(blog_os::test_runner)]
 
-mod vga_buffer;
+#![reexport_test_harness_main = "test_main"]
+
+use core::panic::PanicInfo;
+use blog_os::println;
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     println!("Hello World{}", "!");
 
+    #[cfg(test)]
+    test_main();
+
     loop {}
 }
 
 /// This function is called on panic.
+// our existing panic handler
+#[cfg(not(test))]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     println!("{}", info);
     loop {}
+}
+
+// our panic handler in test mode
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    blog_os::test_panic_handler(info)
 }
